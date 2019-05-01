73236d066809fa6d6134adc834f81550a34d8756
jeremy bouvier <jeremybouvier2b@gmail.com>
2019-05-01 20:43:22 +0200

    refactoring code of UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: jeremy
+ * Date: 16/04/19
+ * Time: 21:43
+ */
 
 namespace Application\Controller;
 
@@ -14,14 +20,9 @@ class UserController extends Controller
      */
     private $displayError;
 
-    /**
-     * @var
-     */
-    private $response;
-
     /**Permet d'editer un nouveau post
      * @param $id
-     * @return string|\Zend\Diactoros\Response\HtmlResponse|\Zend\Diactoros\Response\RedirectResponse
+     * @return string|\Zend\Diactoros\Response\RedirectResponse
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
@@ -30,71 +31,31 @@ class UserController extends Controller
     {
         $user = null;
         $role = null;
-        if ($this->request->getRequest()->getMethod() == "POST" AND $this->tokenVerify()){
+        if ($this->request->getRequest()->getMethod() == "POST"){
             $user = new User();
             $this->displayError = $user->hydrate($this->request->getPost());
             $user->setPassword(password_hash($user->getPassword(), PASSWORD_DEFAULT));
             $user->setPrimaryKey($id);
-            $this->response = $this->formControl($user, $id);
-        }
-        else{
-            if ($id != 0){
-                $user = $this->pre_filledForm($user, $id);
-                $role = $this->getManager( Role::class)->fetch(['id'=>$user->getRoleId()]);
+            if ($this->checkError($this->displayError) == false){
+                $this->getManager(User::class)->edit($user, ['id' => $id]);
+                return $this->redirect('administrationPage', 302);
             }
-            $this->response = $this->displayPage($user, $role);
         }
-        return $this->response;
+        if ($id != 0){
+            if ($user == null){
+                $user = $this->getManager( User::class)->fetch(['id'=>$id]);
+            }
+            $role = $this->getManager( Role::class)->fetch(['id'=>$user->getRoleId()]);
+        }
+        $roleList = $this->getManager(Role::class)->getAll();
+        $data =  [
+            'User'=> $user,
+            'Role' => $role,
+            'roleList' => $roleList,
+            'displayError' => $this->displayError
+        ];
+        $response = $this->render('editUser.twig', $data);
+        return $response;
     }
 
-    /**Controle le remplissage du formulaire d'édition d'un utilisateur
-     * @param $user
-     * @param $id
-     * @return string|\Zend\Diactoros\Response\HtmlResponse|\Zend\Diactoros\Response\RedirectResponse
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    private function formControl($user, $id)
-    {
-        if ($this->checkError($this->displayError) == false){
-            $this->getManager(User::class)->edit($user, ['id' => $id]);
-            return $this->redirect('administrationPage', 302);
-        }
-        $role = $this->getManager( Role::class)->fetch(['id'=>$user->getRoleId()]);
-        return $this->displayPage($user, $role);
-    }
-
-    /**Pré-rempli le formulaire de modification d'un utilisateur
-     * @param $user
-     * @param $id
-     * @return mixed
-     */
-    private function pre_filledForm($user, $id)
-    {
-        if ($user == null){
-            $user = $this->getManager( User::class)->fetch(['id'=>$id]);
-        }
-        return $user;
-    }
-
-    /**créer l'affichage de la page
-     * @param $user
-     * @param null $role
-     * @return string|\Zend\Diactoros\Response\HtmlResponse
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    private function displayPage($user, $role)
-    {
-        return $this->render('editUser.twig',
-            [
-                'User'=> $user,
-                'Role' => $role,
-                'roleList' => $this->getManager(Role::class)->getAll(),
-                'displayError' => $this->displayError,
-                'session' => $_SESSION
-            ]);
-    }
 }
